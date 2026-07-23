2d344e16561d32d05f9acc855d9410abcdedf578
Daniil Mikheev <42416692+Woys@users.noreply.github.com>
2026-07-23 00:43:03 -0400

    Add Trouble

--- init.lua
+++ init.lua
@@ -964,6 +964,19 @@ local plugin_specs = {
     opts = { signs = false },
   },
 
+  {
+    'folke/trouble.nvim',
+    config = function()
+      require('trouble').setup {}
+
+      vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>', { desc = 'Diagnostics (Trouble)' })
+      vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', { desc = 'Buffer diagnostics (Trouble)' })
+      vim.keymap.set('n', '<leader>cs', '<cmd>Trouble symbols toggle focus=false<CR>', { desc = 'Document symbols (Trouble)' })
+      vim.keymap.set('n', '<leader>xq', '<cmd>Trouble qflist toggle<CR>', { desc = 'Quickfix list (Trouble)' })
+      vim.keymap.set('n', '<leader>xl', '<cmd>Trouble loclist toggle<CR>', { desc = 'Location list (Trouble)' })
+    end,
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
